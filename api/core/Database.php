f1e4120098fa372f574a3604e70981bc1418353e
Douglas <133309975+douglas2569@users.noreply.github.com>
2023-07-15 14:49:49 -0300

    Update Database.php

--- api/core/Database.php
+++ api/core/Database.php
@@ -6,8 +6,12 @@ use \src\Config;
 class Database {
     private static $_pdo;
     public static function getInstance() {
+        $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => '/var/www/html/DigiCertGlobalRootG2.crt.pem'
+        );
+                
         if(!isset(self::$_pdo)) {
-            self::$_pdo = new \PDO(Config::DB_DRIVER.":dbname=".Config::DB_DATABASE.";host=".Config::DB_HOST, Config::DB_USER, Config::DB_PASS);
+            self::$_pdo = new \PDO(Config::DB_DRIVER.":dbname=".Config::DB_DATABASE.";host=".Config::DB_HOST.";port=3306", Config::DB_USER, Config::DB_PASS, $options);
         }
         return self::$_pdo;
     }
