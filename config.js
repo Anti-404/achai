ad35f4c5b3ccbabe0e2dcd78a4518fdf9c6d0b39
Douglas <133309975+douglas2569@users.noreply.github.com>
2023-07-15 11:37:11 -0300

    update config

--- config.js
+++ config.js
@@ -4,8 +4,11 @@ let ssl = false;
 host = 'achai2.000webhostapp.com';
 ssl = true;
 
+host = 'achaiachadoseperdidos.azurewebsites.net';
+ssl = true;
+
 const config =  {
-    urlBase:(ssl)?`https://${host}/achai`:`http://${host}/achai`,        
+    urlBase:(ssl)?`https://${host}`:`http://${host}`,        
     
 }
 
